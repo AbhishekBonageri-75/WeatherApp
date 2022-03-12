bcc9de65dfabb2ac7bfd03f81b374dd4ea4da661
AFA21SCM21BO <abonageri@hawk.iit.edu>
2022-03-11 21:57:00 -0600

    activity 2 connected

--- app/src/main/java/com/example/openweatherapp/MainActivity.java
+++ app/src/main/java/com/example/openweatherapp/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.openweatherapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import java.util.Objects;
 
@@ -17,5 +22,30 @@ public class MainActivity extends AppCompatActivity {
         //Changing the action bar colour
         Objects.requireNonNull(getSupportActionBar()).setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.purple_500)));
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.opt_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.opt_temp_format){
+            Toast.makeText(this, "Temp_selected", Toast.LENGTH_SHORT).show();
+        }
+        else if(item.getItemId() == R.id.opt_calendar){
+//            Toast.makeText(this, "Cal_selected", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(MainActivity.this,Activity2.class);
+            startActivity(intent);
+        }
+        else if(item.getItemId() == R.id.opt_location){
+            Toast.makeText(this, "Locaiton_selected", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Toast.makeText(this, "No such selection", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
