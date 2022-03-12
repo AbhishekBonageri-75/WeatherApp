2da1ac5e6f849af7d25814fd8447aafe46286884
AFA21SCM21BO <abonageri@hawk.iit.edu>
2022-03-11 22:46:31 -0600

    units_f to units_c icon change on click implimented

--- app/src/main/java/com/example/openweatherapp/MainActivity.java
+++ app/src/main/java/com/example/openweatherapp/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.openweatherapp;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import java.util.Objects;
@@ -22,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
         //Changing the action bar colour
         Objects.requireNonNull(getSupportActionBar()).setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.purple_500)));
 
+        //Loading json data using volley
+
     }
 
     @Override
@@ -32,20 +39,62 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
         if(item.getItemId() == R.id.opt_temp_format){
             Toast.makeText(this, "Temp_selected", Toast.LENGTH_SHORT).show();
+            int[] images = {R.drawable.units_c,R.drawable.units_f};
+            if(item.getIcon().getConstantState().equals(getResources().getDrawable(R.drawable.units_c).getConstantState())){
+                item.setIcon(R.drawable.units_f);
+            }
+            else if(item.getIcon().getConstantState().equals(getResources().getDrawable(R.drawable.units_f).getConstantState())){
+                item.setIcon(R.drawable.units_c);
+            }
+            else{
+                item.setIcon(R.drawable.units_f);
+            }
+
         }
         else if(item.getItemId() == R.id.opt_calendar){
-//            Toast.makeText(this, "Cal_selected", Toast.LENGTH_SHORT).show();
+
             Intent intent = new Intent(MainActivity.this,Activity2.class);
             startActivity(intent);
         }
         else if(item.getItemId() == R.id.opt_location){
-            Toast.makeText(this, "Locaiton_selected", Toast.LENGTH_SHORT).show();
+            generate_dialogue();
+
         }
         else{
             Toast.makeText(this, "No such selection", Toast.LENGTH_SHORT).show();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+
+
+    public void generate_dialogue(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        final EditText et = new EditText(this);
+        et.setInputType(InputType.TYPE_CLASS_TEXT);
+        et.setGravity(Gravity.CENTER_HORIZONTAL);
+        builder.setView(et);
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+
+                Toast.makeText(MainActivity.this, "Location_selected", Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                Toast.makeText(MainActivity.this, "You changed your mind!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        builder.setMessage("For US : 'City' or 'City,State'\n\nInternational Locations: 'City,Country' ");
+        builder.setTitle("Enter a Location");
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
